1c5e3288b88ab1796944d92e4b20297dc9157402
Khalil Fazal <khalil.fazal@uoit.net>
2017-09-26 21:42:47 -0400

    added Haskell code

---
+++ Main.hs
@@ -0,0 +1,37 @@
+module Main where
+
+import Data.Foldable        (foldl')
+import Data.Sequence        ((<|), (|>), Seq, ViewL ((:<)), empty, fromList, index, length, partition, singleton, spanl, viewl, zipWith)
+import Prelude       hiding (length, zipWith)
+
+ilog2 :: Integral int => int -> Int
+ilog2 = truncate . logBase 2 . fromIntegral
+
+nats :: Integral int => int -> Seq int
+nats = fromList . enumFromTo 1
+
+groupBy :: (a -> a -> Bool) -> Seq a -> Seq (Seq a)
+groupBy eq xs'
+    | null xs'  = empty
+    | otherwise = (x <| ys) <| groupBy eq zs
+        where
+            (x :< xs) = viewl xs'
+            (ys, zs)  = spanl (eq x) xs
+
+group :: Eq a => Seq a -> Seq (Seq a)
+group = groupBy (==)
+
+as :: Integral int => int -> Seq int
+as n = foldl' (\a i -> a |> (i + a `index` ilog2 i + 1)) (singleton 1) [1 .. n]
+
+bs :: Integral int => int -> Seq int
+bs n = (snd . partition (`elem` as n) . nats) n
+
+cs :: Integral int => int -> Seq int
+cs n = (zipWith subtract (nats n) . bs) n
+
+ds :: Integral int => int -> Seq Int
+ds = fmap length . group . cs
+
+main :: IO ()
+main = return ()
